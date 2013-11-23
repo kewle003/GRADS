f1da03191cf08071d1a15bc697c2aa038cba3fd6
kewle003 <markkewley@hotmail.com>
2013-11-23 16:34:30 -0600

    GPA Requirement idea

--- src/edu/umn/csci5801/model/GPARequirement.java
+++ src/edu/umn/csci5801/model/GPARequirement.java
@@ -1,5 +1,6 @@
 package edu.umn.csci5801.model;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -7,11 +8,11 @@ import java.util.List;
 public class GPARequirement extends Requirement {
     //Overall GPA, Breadth GPA, In-program GPA
     private String name;
-    private CourseArea area;
+    private Program program;
 
-    public GPARequirement(String name, CourseArea area) {
+    public GPARequirement(String name, Program program) {
         super(name);
-        this.area = area;
+        this.program = program;
     }
     
     //TODO: Add in System Crash handling
@@ -34,8 +35,41 @@ public class GPARequirement extends Requirement {
     @Override
     public RequirementCheckResult metBy(StudentRecord studentRecord) {
         RequirementCheckResult requirement = new RequirementCheckResult(name);
-        
-        return null;
+        List<CourseTaken> courses = studentRecord.getCoursesTaken();
+        //Do some logic to fetch courses
+        if (name.equals("Breadth-GPA")) {
+            //Loop through courses extracting only BREADTH
+            if(program.equals(Degree.PHD)) {
+                if (3.2 <= calculateGPA(courses)) {
+                    requirement.setPassed(true);
+                } else {
+                    requirement.setPassed(false);
+                    List<String> errMessage = new ArrayList<String>();
+                    errMessage.add("Breadth-GPA is not high enough");
+                    requirement.setErrorMsgs(errMessage);
+                }
+            } else {
+                if (3.0 <= calculateGPA(courses)) {
+                    requirement.setPassed(true);
+                } else {
+                    requirement.setPassed(false);
+                }
+            }
+        } else if (name.equals("In-Program-GPA")) {
+            //Loop through courses extracting only IN-PROGRAM-GPA
+            if (3.0 <= calculateGPA(courses)) {
+                requirement.setPassed(true);
+            } else {
+                requirement.setPassed(false);
+            }
+        } else if (name.equals("Overall-GPA")) {
+            if (3.0 <= calculateGPA(courses)) {
+                requirement.setPassed(true);
+            } else {
+                requirement.setPassed(false);
+            }
+        }
+        return requirement;
     }
 
 }
