e868cd292aa07527b526916f99e150a66dd14932
Kevin Viratyosin <virat001@umn.edu>
2013-11-23 12:56:53 -0600

    wrote generateProgressSummary

--- src/edu/umn/csci5801/model/ProgressSummaryBuilder.java
+++ src/edu/umn/csci5801/model/ProgressSummaryBuilder.java
@@ -7,16 +7,28 @@ import java.util.List;
 public class ProgressSummaryBuilder {
     private List<Requirement> requirements;
     private HashMap<Degree,Program> programs;
-    private List<Course> courses; // TODO add to Design doc
     
-    public ProgressSummaryBuilder(List<Course> courses) {
-        this.courses = courses;
+    public ProgressSummaryBuilder() {
         this.requirements = new ArrayList<Requirement>();
         this.programs = new HashMap<Degree,Program>();
     }
     
     public ProgressSummary generateProgressSummary(StudentRecord studentRecord) {
-        return null;
+        ProgressSummary progressSummary = new ProgressSummary();
+        progressSummary.setStudent(studentRecord.getStudent());
+        progressSummary.setDepartment(studentRecord.getDepartment());
+        progressSummary.setDegreeSought(studentRecord.getDegreeSought());
+        progressSummary.setTermBegan(studentRecord.getTermBegan());
+        progressSummary.setAdvisors(studentRecord.getAdvisors());
+        progressSummary.setCommittee(studentRecord.getCommittee());
+        progressSummary.setNotes(studentRecord.getNotes());
+        
+        List<Requirement> requirements = this.programs.get(studentRecord.getDegreeSought()).getRequirements();
+        for (Requirement requirement: requirements) {
+            progressSummary.addRequirementResult(requirement.metBy(studentRecord));
+        }
+        
+        return progressSummary;
     }
     
     public List<Program> getPrograms() {
