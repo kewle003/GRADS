f1442bdb5368dc4feca902e19fd27ff51d1045e8
Kevin Viratyosin <virat001@umn.edu>
2013-12-04 19:19:55 -0600

    Began requirements: MS Breadth Requirement

--- src/edu/umn/csci5801/model/GPARequirement.java
+++ src/edu/umn/csci5801/model/GPARequirement.java
@@ -5,14 +5,22 @@ import java.util.Iterator;
 import java.util.List;
 
 
-public class GPARequirement extends Requirement {
-    //Overall GPA, Breadth GPA, In-program GPA
-    private String name;
-    private Program program;
-
-    public GPARequirement(String name, Program program) {
+public abstract class GPARequirement extends Requirement {
+    
+    public GPARequirement(String name) {
         super(name);
-        this.program = program;
+    }
+
+    // TODO add to design
+    // returns CourseTaken with highest grade
+    public static CourseTaken getBestCourseTaken(List<CourseTaken> courses) {
+        CourseTaken best = null;
+        for (CourseTaken course: courses) {
+            if ( best == null || course.getGrade().numericValue() > best.getGrade().numericValue() ) {
+                best = course;
+            }
+        }
+        return best;
     }
     
     //TODO: Add in System Crash handling
@@ -30,46 +38,6 @@ public class GPARequirement extends Requirement {
             }
         }
         return (gradeSum/creditSum);
-    }
-
-    @Override
-    public RequirementCheckResult metBy(StudentRecord studentRecord) {
-        RequirementCheckResult requirement = new RequirementCheckResult(name);
-        List<CourseTaken> courses = studentRecord.getCoursesTaken();
-        //Do some logic to fetch courses
-        if (name.equals("Breadth-GPA")) {
-            //Loop through courses extracting only BREADTH
-            if(program.equals(Degree.PHD)) {
-                if (3.2 <= calculateGPA(courses)) {
-                    requirement.setPassed(true);
-                } else {
-                    requirement.setPassed(false);
-                    List<String> errMessage = new ArrayList<String>();
-                    errMessage.add("Breadth-GPA is not high enough");
-                    requirement.setErrorMsgs(errMessage);
-                }
-            } else {
-                if (3.0 <= calculateGPA(courses)) {
-                    requirement.setPassed(true);
-                } else {
-                    requirement.setPassed(false);
-                }
-            }
-        } else if (name.equals("In-Program-GPA")) {
-            //Loop through courses extracting only IN-PROGRAM-GPA
-            if (3.25 <= calculateGPA(courses)) {
-                requirement.setPassed(true);
-            } else {
-                requirement.setPassed(false);
-            }
-        } else if (name.equals("Overall-GPA")) {
-            if (3.0 <= calculateGPA(courses)) {
-                requirement.setPassed(true);
-            } else {
-                requirement.setPassed(false);
-            }
-        }
-        return requirement;
     }
 
 }
